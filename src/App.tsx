f3b8bed6a438e08950f70045b3af4785b437a468
˜huelton˜ <hueltondihon@gmail.com>
2023-09-29 16:08:22 -0700

    Add Cart page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import Catalog from "./routes/ClientHome/Catalog";
 import ClientHome from "./routes/ClientHome";
+import Cart from "./routes/ClientHome/Cart";
 
 export default function App() {
   return (
@@ -12,7 +13,8 @@ export default function App() {
           <Route path="/" element={<ClientHome />}>
             <Route index element={<Catalog />} />
             <Route path="catalog" element={<Catalog />} />
-            <Route path="product-details/:productId" element={<ProductDetails />} />            
+            <Route path="product-details/:productId" element={<ProductDetails />} />     
+            <Route path="cart" element={<Cart />} />       
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
