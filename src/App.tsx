022a9e28253fb54c7eb73faffb9f8c28b5a88f33
˜huelton˜ <hueltondihon@gmail.com>
2023-10-01 07:51:25 -0700

    create Login component refatoring requestBackend

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ClientHome from "./routes/ClientHome";
 import Cart from "./routes/ClientHome/Cart";
 import { useState } from "react";
 import { ContextCartCount } from "./utils/context-cart";
+import Login from "./routes/ClientHome/Login";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -25,6 +26,7 @@ export default function App() {
                 element={<ProductDetails />}
               />
               <Route path="cart" element={<Cart />} />
+              <Route path="login" element={<Login />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
