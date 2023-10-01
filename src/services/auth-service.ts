f76003625ae728695c58603c8fd32f566ee75112
˜huelton˜ <hueltondihon@gmail.com>
2023-10-01 11:31:48 -0700

    Admin page layout

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,22 +3,37 @@ import { CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import * as accessTokenRepository from "../localstorage/access-token-repository";
 
-export function loginRequest(loginData: CredentialsDTO){
+export function loginRequest(loginData: CredentialsDTO) {
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+  };
 
-    const headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": "Basic " + window.btoa(CLIENT_ID + ":"+ CLIENT_SECRET),
-    }
+  const requestBody = QueryString.stringify({
+    ...loginData,
+    grant_type: "password",
+  });
 
-    const requestBody = QueryString.stringify({...loginData, grant_type: "password"}); 
+  const config: AxiosRequestConfig = {
+    method: "POST",
+    url: "/oauth2/token",
+    data: requestBody,
+    headers,
+  };
 
-    const config: AxiosRequestConfig = {
-        method: "POST",
-        url: "/oauth2/token",
-        data: requestBody,
-        headers
-    }
-    
-    return requestBackend(config);
+  return requestBackend(config);
+}
+
+export function logout() {
+  accessTokenRepository.remove();
+}
+
+export function saveAccessToken(token: string) {
+  accessTokenRepository.save(token);
+}
+
+export function getAccessToken() {
+  accessTokenRepository.get();
 }
--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,4 +1,5 @@
 export const BASE_URL= "http://localhost:8081";
 export const CART_KEY= "com.dsecommerce/Cart";
+export const TOKEN_KEY= "com.dsecommerce/Token";
 export const CLIENT_ID= "myclientid";
 export const CLIENT_SECRET= "myclientsecret";
